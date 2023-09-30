797a4e20568fbfc28ef673f43b7d2ac761442d91
Дмитрий <d77s@inbox.ru>
2023-09-30 19:07:56 +0300

    Next

--- news/pytest_tests/test_logic.py
+++ news/pytest_tests/test_logic.py
@@ -6,11 +6,24 @@ from news.models import Comment
 
 
 @pytest.mark.django_db
-def test_anonymous_user_cant_create_comment(id_for_args, client):
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('client'), True),  # type: ignore
+        (pytest.lazy_fixture('admin_client'), False)  # type: ignore
+    ),
+)
+def test_anonymous_user_cant_create_comment(
+    id_for_args,
+    parametrized_client,
+    expected_status
+):
     '''
-    Проверяет, что анонимус не может отправить коммент.'''
+    Проверяет, что комментарий:
+    - не может быть отправлен анонимусом,
+    - может быть отправлен залогированным.'''
     url_of_novost_to_comment_to = reverse('news:detail', args=id_for_args)
-    form_data = {'text': 'Текст попытки анонима комментировать.'}
-    client.post(url_of_novost_to_comment_to, data=form_data)
+    form_data = {'text': 'Текст попытки комментировать.'}
+    parametrized_client.post(url_of_novost_to_comment_to, data=form_data)
     comments_count = Comment.objects.count()
-    assert comments_count == 0
+    assert (comments_count == 0) is expected_status
