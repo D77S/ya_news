b17de83b25eedd5eb43e557c81df632e5db5976f
Дмитрий <d77s@inbox.ru>
2023-09-30 19:49:22 +0300

    Next

--- news/pytest_tests/test_logic.py
+++ news/pytest_tests/test_logic.py
@@ -1,8 +1,10 @@
 import pytest
+from pytest_django.asserts import assertFormError
 
 from django.urls import reverse
 
 from news.models import Comment
+from news.forms import BAD_WORDS, WARNING
 
 
 @pytest.mark.django_db
@@ -27,3 +29,17 @@ def test_anonymous_user_cant_create_comment(
     parametrized_client.post(url_of_novost_to_comment_to, data=form_data)
     comments_count = Comment.objects.count()
     assert (comments_count == 0) is expected_status
+
+
+@pytest.mark.django_db
+def test_user_cant_use_bad_words(id_for_args, novost, admin_client):
+    '''
+    Проверяет, что если камент содержит
+    запрещенные слова, то он не будет опубликован,
+    а форма вернет ошибку.'''
+    bad_words_data = {'text': f'Какой-то текст, {BAD_WORDS[0]}, еще текст'}
+    url_of_novost_to_comment_to = reverse('news:detail', args=id_for_args)
+    response = admin_client.post(url_of_novost_to_comment_to, data=bad_words_data)
+    assertFormError(response, 'form', 'text', errors=(WARNING))
+    comments_count = Comment.objects.count()
+    assert comments_count == 0
