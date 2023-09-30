c5e8c8b4d477b4fe87b28c735bb14b0b502d6a3e
Дмитрий <d77s@inbox.ru>
2023-09-30 15:20:45 +0300

    Next

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -30,11 +30,33 @@ def test_pages_availability_for_anonim(client, name, args) -> None:
     assert response.status_code == HTTPStatus.OK
 
 
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture(
+            'admin_client'
+        ), HTTPStatus.NOT_FOUND),  # type: ignore
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)  # type: ignore
+    ),
+)
 @pytest.mark.parametrize(
         'name',
         ('news:edit', 'news:delete'),
 )
-def test_comment_change_pages_for_author(author_client, name, comment):
+def test_comment_change_pages_for_author(
+    parametrized_client,
+    name,
+    comment,
+    expected_status
+):
+    '''
+    Проверяет, что НЕавтору камента НЕдоступно:
+    - его редактирование,
+    - его удаление.
+    А вот автору камента доступно:
+    - его редактирование,
+    - его удаление.'''
     url = reverse(name, args=(comment.id,))
-    response = author_client.get(url)
-    assert response.status_code == HTTPStatus.OK
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
